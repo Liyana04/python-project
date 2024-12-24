e65b41194c0ffbbb918576637162aea018cb15fa
Liyana04 <liyanaaris04@gmail.com>
2024-12-24 16:59:46 +0800

    practise python

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 print("Nur Liyana")
 # can put "" or'' 
+# python is capital/lowe case sensitive
 print('*' * 10)
 price = 10
 price = 20
+is_true = False
 print(price)
+
+name = input('What is your name? ')
+color = input('What is your favourite color? ')
+print('Hi ' + name + ' ,you like ' + color + 'color')
