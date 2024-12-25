9b5caaa6347f9cde465a51eece31fed9ab51a672
Liyana04 <liyanaaris04@gmail.com>
2024-12-25 11:34:02 +0800

    practise python

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ price = 20
 is_true = False
 print(price)
 
+# new practise
 # name = input('What is your name? ')
 # color = input('What is your favourite color? ')
 # print('Hi ' + name + ' ,you like ' + color + 'color')
 
+# new practise
 # birth_year = input('Birth year: ')
 # print(type(birth_year))
 # age = 2024 -int(birth_year)
@@ -20,10 +22,12 @@ print(price)
 # # float()
 # print(age)
 
+# new practise
 # weight_lbs = input('Weight(lbs): ')
 # weight_kg = int(weight_lbs) * 0.45
 # print(weight_kg)
 
+# new practise
 course = "Python's Course for Beginners"
 # or can write like this
 course_1 ='Python Course for "Beginners"'
@@ -36,5 +40,16 @@ Hello John,
 Regards,
 Yana
 '''
+another = course[:]
+name = 'Jennifer'
+# print(name[1:-1]) will output ennife  
 print(course)
 # print(course[0]) will output P or course[-1] will output s or course[0:3] will output Pyt
+
+# formatted string practise
+first = 'John'
+last = 'Smith'
+msg = f'{first} [{last}] is a coder'
+print(msg)
+
+
