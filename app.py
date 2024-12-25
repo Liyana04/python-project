06e739859ca765c083a3c27f53223bb8c0759669
Liyana04 <liyanaaris04@gmail.com>
2024-12-25 11:44:29 +0800

    practise python

--- app.py
+++ app.py
@@ -28,28 +28,41 @@ print(price)
 # print(weight_kg)
 
 # new practise
-course = "Python's Course for Beginners"
-# or can write like this
-course_1 ='Python Course for "Beginners"'
-# or can write like this for multiple line
-course_2 ='''
-Hello John,
+# course = "Python's Course for Beginners"
+# # or can write like this
+# course_1 ='Python Course for "Beginners"'
+# # or can write like this for multiple line
+# course_2 ='''
+# Hello John,
 
- Here is our first email.
+#  Here is our first email.
 
-Regards,
-Yana
-'''
-another = course[:]
-name = 'Jennifer'
-# print(name[1:-1]) will output ennife  
-print(course)
-# print(course[0]) will output P or course[-1] will output s or course[0:3] will output Pyt
+# Regards,
+# Yana
+# '''
+# another = course[:]
+# name = 'Jennifer'
+# # print(name[1:-1]) will output ennife  
+# print(course)
+# # print(course[0]) will output P or course[-1] will output s or course[0:3] will output Pyt
 
 # formatted string practise
-first = 'John'
-last = 'Smith'
-msg = f'{first} [{last}] is a coder'
-print(msg)
+# first = 'John'
+# last = 'Smith'
+# msg = f'{first} [{last}] is a coder'
+# print(msg)
+
+# string methods
+course = 'Python for Beginners'
+print(len(course))
+# for upper case print(course.upper())
+# for lower case print(course.lower())
+# for finding the index of the character print(course.find('P')) will output 0
+# for finding the index of the character print(course.find('Beginners')) will output 11, start with 'B'
+# for replace 'Beginners' to 'Absolute Beginners' print(course.replace('Beginners', 'Absolute Beginners'))
+# for checking if 'Python' exist in course? print('Python' in course)
+
+
+
 
 
