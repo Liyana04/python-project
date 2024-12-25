64a9c9498c2561f3f7fc4cbe6a832cb581dca6b6
Liyana04 <liyanaaris04@gmail.com>
2024-12-25 14:16:10 +0800

    practise python

--- app.py
+++ app.py
@@ -68,6 +68,19 @@ print(10/3)
 print(10//3)
 # will return integer of the first number in float answer
 
+# if statements
+is_hot = False
+is_cold = True
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water :)")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes :)")
+else:
+    print('It is a lovely day!')
+    print('Enjoy your day!')
+
 
 
 
