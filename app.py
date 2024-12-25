842be87082f4a922b903c61f50280a125989de8c
Liyana04 <liyanaaris04@gmail.com>
2024-12-25 15:02:14 +0800

    practise python

--- app.py
+++ app.py
@@ -113,8 +113,15 @@ if len(name) > 50:
 else:
     print("Name looks good!")
 
-
-
+# weight converter program
+weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pound")
 
 
 
