cb18c0b9796978cf833ad2985e42332087d50a0f
Liyana04 <liyanaaris04@gmail.com>
2024-12-25 14:19:37 +0800

    practise python

--- app.py
+++ app.py
@@ -81,6 +81,14 @@ else:
     print('It is a lovely day!')
     print('Enjoy your day!')
 
+price = 100000
+has_good_credit = True
+if has_good_credit:
+    down_payment = 0.1 * price
+else
+    down_payment = 0.2 * price
+print(f'Down payment: ${down_payment}')
+
 
 
 
