56ca17de1b65b9c170a513db527d68b7f1d80195
Liyana04 <liyanaaris04@gmail.com>
2024-12-26 10:26:35 +0800

    practise car game python

--- app.py
+++ app.py
@@ -145,11 +145,20 @@ else:
 
 #car game
 command = ""
+started = False
 while True:
     command = input("> ").lower()
     if command == "start":
-        print("Car started...")
+        if started:
+            print("Car already started")
+        else:
+            started = True
+            print("Car started...")
     elif command == "stop":
+        if not started:
+            print("Car already stopped")
+        else:
+            started = False
         print("Car sopped...")
     elif command == "help":
         # python is case sensitive and space sensitive
