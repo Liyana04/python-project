ea903c5f0de5d2dec85f06c5e9833d42dbf53006
Liyana04 <liyanaaris04@gmail.com>
2024-12-26 11:19:54 +0800

    practise for loop python

--- app.py
+++ app.py
@@ -173,7 +173,17 @@ quit - to quit
         print("Sorry, I don't understand that")
 
 #for loops
-
+# for item in range(5, 10, 2):
+# for item in 'Python':
+# for item in['Mosh', 'John', 'Sarah']
+# for item in [1,2,3,4,5,6,7,8,9]:
+# for item in range (10):
+# for item in range(5,10,2): 2 means two numbers after 5 which is 5,7,9
+prices =[10,20,30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
 
 
 
