adaa0f93669c21af41700f30ade43bc750f7559a
Liyana04 <liyanaaris04@gmail.com>
2024-12-26 10:24:21 +0800

    practise car game python

--- app.py
+++ app.py
@@ -144,9 +144,24 @@ else:
     print('Sorry you failed!')
 
 #car game
-
-
-
+command = ""
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car sopped...")
+    elif command == "help":
+        # python is case sensitive and space sensitive
+        print('''
+start - to start the car
+stop - to stop the car
+quit - to quit
+        ''')
+    elif command == quit:
+        break
+    else:
+        print("Sorry, I don't understand that")
 
 
 
