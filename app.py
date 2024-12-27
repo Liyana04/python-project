bd0405906583411935d8e3265a7ab80e59bc3127
Liyana04 <liyanaaris04@gmail.com>
2024-12-27 09:50:39 +0800

    practise constructors in python

--- app.py
+++ app.py
@@ -359,3 +359,36 @@ point1.y = 20
 print(point1.x)
 point1.draw()
 point1 = Point()
+
+# constructors
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def move(self):
+        print("move")
+    def draw(self):
+        print("draw")
+
+
+point=Point(10,20)
+print(point.x)
+# return 10
+point.x =11
+print(point.x)
+# return 11
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print("talk")
+        print(f"Hi!, I' am {self.name}")
+
+
+john = Person("John Smith")
+# print(john.name)
+john.talk()
+
+bob = Person("Bob Joe")
+bob.talk()
