041a7587c40e6260a48d7f7d04715c6d815c7aaa
Liyana04 <liyanaaris04@gmail.com>
2024-12-27 09:37:31 +0800

    practise class in python

--- app.py
+++ app.py
@@ -346,3 +346,16 @@ except ValueError:
     print('Invalid Value!')
 
 #class
+class Point:
+    def move(self):
+        print("move")
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+point1.draw()
+point1 = Point()
